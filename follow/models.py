6069d5fdd1d366a4737932ae716c100b68d9735b
adammck <adam.mckaig@gmail.com>
2009-08-24 07:56:37 -0400

    added generated follow objects to the django admin

--- follow/models.py
+++ follow/models.py
@@ -9,7 +9,7 @@ from apps.reporters.models import Reporter, PersistantConnection
 
 for model in followable_models():
     m_name = "Following%s" % model.__name__
-
+    
     # believe it or not, this is how django generates the field names for
     # many-to-one (reverse foreign key) relationships. so i'm doing the same.
     # see: django.db.models.fields.related.RelatedField.contribute_to_class
@@ -24,6 +24,26 @@ for model in followable_models():
         # always be disallowed in app.py
         "reporter": models.ForeignKey(Reporter, null=True, related_name=r_name),
         "connection": models.ForeignKey(PersistantConnection, null=True, related_name=r_name),
+
+        # replace the verbose name, to shorten the
+        # names in the admin. this will probably
+        # be useful for other things, too...
+        "Meta": type("Meta", (object,), {
+            "verbose_name": model._meta.verbose_name
+        }),
+
+        # for debugging in the console
+        "__repr__": lambda self:\
+            "<%s: %r by %r>" % (
+                type(self).__name__,
+                getattr(self, type(self)._meta._fields()[1].name),
+                self.reporter or self.connection),
+
+        # for viewing in the django admin
+        "__unicode__": lambda self:\
+            u"%s following %s" % (
+                self.reporter or self.connection,
+                getattr(self, type(self)._meta._fields()[1].name)),
 
         # these are necessary to let django know which app this dynamic
         # class lives within. i don't quite understand it, but i suspect
