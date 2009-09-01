c1c8afc09c56323e1cdb94002d18e22da65155d2
adammck <adam.mckaig@gmail.com>
2009-09-01 07:09:02 -0400

    Reporters are now direct messagable
    this is new functionality (any model can be followable and/or
    messagable, not just reporters), but i've attempted to keep the syntax
    the same as the mctc app.
    
      ~~> i am adammck
      <~~ hello, Adam Mckaig!
      ~~> @ewheeler Hello
      <~~ Your message was sent to Evan Wheeler
    
    meanwhile...
    
      ~~> i am ewheeler
      <~~ hello, Evan Wheeler!
      <~~ Adam Mckaig: Hello

--- follow/admin.py
+++ follow/admin.py
@@ -2,9 +2,10 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-import django
+from django.db import models
 from django.contrib import admin
 import apps.follow.models
 
-for m in django.db.models.loading.get_models(apps.follow.models):
+
+for m in models.loading.get_models(apps.follow.models):
     admin.site.register(m)
