10ae53f5f57be272d82b82b81fbc4ad878ffc0b6
adammck <adam.mckaig@gmail.com>
2009-09-12 14:35:17 -0400

    ported (sort of) weight matching from fuzz
    added a handler with a demo in rwanda/child_report

---
+++ rwanda/handlers/child_report.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+from apps.handlers import KeywordHandler
+from rapidsms.search import find_objects
+from apps.reporting.utils import get_weight
+
+
+class ChildReport(KeywordHandler):
+    keyword = "childreport|crep"
+
+    def help(self):
+        self.respond("To report on a child, repond CREP <WEIGHT> <TAGS>")
+
+    def _extract_weight(self):
+        self.weight, remainder = get_weight(text)
+
+    def handle(self, text):
+        kg, text = get_weight(text)
+        if kg is not None:
+            self.respond("Child Report: %d kg, tags: %s" %
+                (kg, ", ".join(map(unicode, self.msg.tags))))
+
+        else:
+            self.help()
